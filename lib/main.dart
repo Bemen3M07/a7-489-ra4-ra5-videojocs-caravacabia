bf5cde4d1f35b33991dbe6f5f6387f0315cd2101
no-data-no-plan <mabi24@bemen3.cat>
2026-04-20 20:46:56 +0200

    Hecho main con fondo animado y hero

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,67 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';                                                                                                                                                           
+import 'package:flame/input.dart';
+import 'package:flame/parallax.dart';                                                                                                                                                       
+import 'package:flutter/material.dart';                                                                                                                                                     
+  
+void main() {                                                                                                                                                                               
+  runApp(GameWidget(game: SpaceShooterGame()));                 
+}                                                                                                                                                                                           
+  
+class SpaceShooterGame extends FlameGame with PanDetector {                                                                                                                                 
+  late Player player;                                           
+                                                                                                                                                                                            
+  @override                                                     
+  Future<void> onLoad() async {
+    final parallax = await loadParallaxComponent(                                                                                                                                           
+      [                                                                                                                                                                                     
+        ParallaxImageData('stars_0.png'),                                                                                                                                                   
+        ParallaxImageData('stars_1.png'),                                                                                                                                                   
+        ParallaxImageData('stars_2.png'),                       
+      ],                                                                                                                                                                                    
+      baseVelocity: Vector2(0, -5),                             
+      repeat: ImageRepeat.repeat,                                                                                                                                                           
+      velocityMultiplierDelta: Vector2(0, 5),                                                                                                                                               
+    );                                                                                                                                                                                      
+    add(parallax);                                                                                                                                                                          
+                                                                                                                                                                                            
+    player = Player();                                          
+    add(player);                                                                                                                                                                            
+  }                                                             
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
+  void onPanUpdate(DragUpdateInfo info) {
+    player.move(info.delta.global);
+  }                                                                                                                                                                                         
 }
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+                                                                                                                                                                                            
+class Player extends SpriteAnimationComponent                   
+    with HasGameReference<SpaceShooterGame> {
+  Player()                                                                                                                                                                                  
+      : super(
+          size: Vector2(100, 150),                                                                                                                                                          
+          anchor: Anchor.center,                                
+        );
+                                                                                                                                                                                            
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Hello World'),
-      ),
-      body: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+  Future<void> onLoad() async {                                                                                                                                                             
+    await super.onLoad();                                       
+
+    animation = await game.loadSpriteAnimation(
+      'player.png',
+      SpriteAnimationData.sequenced(
+        amount: 4,                                                                                                                                                                          
+        stepTime: 0.2,
+        textureSize: Vector2(32, 48),                                                                                                                                                       
+      ),                                                        
     );
-  }
-}
+                                                                                                                                                                                            
+    position = game.size / 2;
+  }                                                                                                                                                                                         
+                                                                
+  void move(Vector2 delta) {                                                                                                                                                                
+    position.add(delta);
+  }                                                                                                                                                                                         
+}                 
