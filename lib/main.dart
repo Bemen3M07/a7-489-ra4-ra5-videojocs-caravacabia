77d1e70b3233110cff271529a6d477703699294c
dacaga-hub <daniel.caravacagarcia@gmail.com>
2026-04-20 20:54:17 +0200

    codigo actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,101 @@
+  import 'package:flame/collisions.dart';
+  import 'package:flame/components.dart';
+  import 'package:flame/events.dart';
+  import 'package:flame/experimental.dart';
+  import 'package:flame/game.dart';
+  import 'package:flame/input.dart';
+  import 'package:flame/parallax.dart';
+  import 'package:flutter/material.dart';
 
-import 'package:flame/components.dart';
-import 'package:flame/events.dart';
-import 'package:flame/game.dart';                                                                                                                                                           
-import 'package:flame/input.dart';
-import 'package:flame/parallax.dart';                                                                                                                                                       
-import 'package:flutter/material.dart';                                                                                                                                                     
-  
-void main() {                                                                                                                                                                               
-  runApp(GameWidget(game: SpaceShooterGame()));                 
-}                                                                                                                                                                                           
-  
-class SpaceShooterGame extends FlameGame with PanDetector {                                                                                                                                 
-  late Player player;                                           
-                                                                                                                                                                                            
-  @override                                                     
-  Future<void> onLoad() async {
-    final parallax = await loadParallaxComponent(                                                                                                                                           
-      [                                                                                                                                                                                     
-        ParallaxImageData('stars_0.png'),                                                                                                                                                   
-        ParallaxImageData('stars_1.png'),                                                                                                                                                   
-        ParallaxImageData('stars_2.png'),                       
-      ],                                                                                                                                                                                    
-      baseVelocity: Vector2(0, -5),                             
-      repeat: ImageRepeat.repeat,                                                                                                                                                           
-      velocityMultiplierDelta: Vector2(0, 5),                                                                                                                                               
-    );                                                                                                                                                                                      
-    add(parallax);                                                                                                                                                                          
-                                                                                                                                                                                            
-    player = Player();                                          
-    add(player);                                                                                                                                                                            
-  }                                                             
+  void main() {
+    runApp(GameWidget(game: SpaceShooterGame()));
+  }
 
-  @override
-  void onPanUpdate(DragUpdateInfo info) {
-    player.move(info.delta.global);
-  }                                                                                                                                                                                         
-}
-                                                                                                                                                                                            
-class Player extends SpriteAnimationComponent                   
-    with HasGameReference<SpaceShooterGame> {
-  Player()                                                                                                                                                                                  
-      : super(
-          size: Vector2(100, 150),                                                                                                                                                          
-          anchor: Anchor.center,                                
-        );
-                                                                                                                                                                                            
-  @override
-  Future<void> onLoad() async {                                                                                                                                                             
-    await super.onLoad();                                       
+  class SpaceShooterGame extends FlameGame
+      with PanDetector, HasCollisionDetection {
+    late Player player;
 
-    animation = await game.loadSpriteAnimation(
-      'player.png',
-      SpriteAnimationData.sequenced(
-        amount: 4,                                                                                                                                                                          
-        stepTime: 0.2,
-        textureSize: Vector2(32, 48),                                                                                                                                                       
-      ),                                                        
-    );
-                                                                                                                                                                                            
-    position = game.size / 2;
-  }                                                                                                                                                                                         
-                                                                
-  void move(Vector2 delta) {                                                                                                                                                                
-    position.add(delta);
-  }                                                                                                                                                                                         
-}                 
+    @override
+    Future<void> onLoad() async {
+      final parallax = await loadParallaxComponent(
+        [
+          ParallaxImageData('stars_0.png'),
+          ParallaxImageData('stars_1.png'),
+          ParallaxImageData('stars_2.png'),
+        ],
+        baseVelocity: Vector2(0, -5),
+        repeat: ImageRepeat.repeat,
+        velocityMultiplierDelta: Vector2(0, 5),
+      );
+      add(parallax);
+
+      player = Player();
+      add(player);
+
+      add(
+        SpawnComponent(
+          factory: (index) {
+            return Enemy();
+          },
+          period: 1,
+          area: Rectangle.fromLTWH(0, 0, size.x, -Enemy.enemySize),
+        ),
+      );
+    }
+
+    @override
+    void onPanUpdate(DragUpdateInfo info) {
+      player.move(info.delta.global);
+    }
+
+    @override
+    void onPanStart(DragStartInfo info) {
+      player.startShooting();
+    }
+
+    @override
+    void onPanEnd(DragEndInfo info) {
+      player.stopShooting();
+    }
+  }
+
+  class Player extends SpriteAnimationComponent
+      with HasGameReference<SpaceShooterGame> {
+    Player()
+        : super(
+            size: Vector2(100, 150),
+            anchor: Anchor.center,
+          );
+
+    late final SpawnComponent _bulletSpawner;
+
+    @override
+    Future<void> onLoad() async {
+      await super.onLoad();
+
+      animation = await game.loadSpriteAnimation(
+        'player.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.2,
+          textureSize: Vector2(32, 48),
+        ),
+      );
+
+      position = game.size / 2;
+
+      game.add(_bulletSpawner);
+    }
+
+    void move(Vector2 delta) {
+      position.add(delta);
+    }
+
+    void startShooting() {
+      _bulletSpawner.timer.start();
+    }
+
+    void stopShooting() {
+      _bulletSpawner.timer.stop();
+    }
+  }
